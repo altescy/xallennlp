1bf9d01d698a3e5c6fc6172905ef0c0c3f53db5b
altescy <altescy@fastmail.com>
2020-06-21 03:45:31 +0900

    add mlflow experiment setting

--- xallennlp/commands/train_with_mlflow.py
+++ xallennlp/commands/train_with_mlflow.py
@@ -28,6 +28,14 @@ class TrainWithMLflow(Subcommand):
             help="path to parameter file describing the model to be trained")
 
         subparser.add_argument(
+            "-e",
+            "--experiment",
+            type=str,
+            default="",
+            help="name of mlflow experiment",
+        )
+
+        subparser.add_argument(
             "-s",
             "--serialization-dir",
             type=str,
@@ -93,6 +101,9 @@ def train_model_from_args(args: argparse.Namespace):
     params_dict = params.as_flat_dict()
     params_dict.update({"args": vars(args)})
     flattened_params = flatten_dict_for_mlflow_log(params_dict)
+
+    if args.experiment:
+        mlflow.set_experiment(args.experiment)
 
     with mlflow.start_run():
         mlflow.log_params(flattened_params)
