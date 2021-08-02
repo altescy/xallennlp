9d4e413629284d499a235850591121d2f69764dc
Yasuhiro Yamaguchi <altescy@fastmail.com>
2021-08-02 16:30:40 +0900

    add compose seq2vec encoder

---
+++ xallennlp/modules/seq2vec_encoders/__init__.py
@@ -0,0 +1,1 @@
+from xallennlp.modules.seq2vec_encoders.compose_seq2vec_encoder import ComposeSeq2VecEncoder  # noqa: F401
---
+++ xallennlp/modules/seq2vec_encoders/compose_seq2vec_encoder.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+import torch
+from allennlp.modules.feedforward import FeedForward
+from allennlp.modules.seq2seq_encoders import Seq2SeqEncoder
+from allennlp.modules.seq2vec_encoders import Seq2VecEncoder
+
+
+@Seq2VecEncoder.register("compose")
+class ComposeSeq2VecEncoder(Seq2VecEncoder):
+    def __init__(
+        self,
+        seq2seq_encoder: Seq2SeqEncoder,
+        seq2vec_encoder: Seq2VecEncoder,
+        feedforward: Optional[FeedForward] = None,
+    ) -> None:
+        super().__init__()
+        self._seq2seq_encoder = seq2seq_encoder
+        self._seq2vec_encoder = seq2vec_encoder
+        self._feedforward = feedforward
+
+    def forward(
+        self,
+        inputs: torch.Tensor,
+        mask: Optional[torch.BoolTensor] = None,
+    ) -> torch.Tensor:
+        output = inputs
+
+        output = self._seq2seq_encoder(output, mask)
+        output = self._seq2vec_encoder(output, mask)
+
+        if self._feedforward is not None:
+            output = self._feedforward(output)
+
+        return output
