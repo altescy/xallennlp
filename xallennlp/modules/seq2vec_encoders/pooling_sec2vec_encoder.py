711cd586b9d287d2da2716b23fb764eb670f7616
altescy <altescy@fastmail.com>
2021-08-27 16:35:40 +0900

    add pooling seq2vec encoder

---
+++ xallennlp/modules/seq2vec_encoders/pooling_sec2vec_encoder.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+import torch
+from allennlp.modules.seq2vec_encoders import Seq2VecEncoder
+from xallennlp.utils import masked_pool
+
+
+@Seq2VecEncoder.register("pooling")
+class PoolingSeq2VecEncoder(Seq2VecEncoder):
+    def __init__(
+        self,
+        input_dim: int,
+        method: str = "average",
+    ) -> None:
+        super().__init__()
+        self._input_dim = input_dim
+        self._method = method
+
+    def get_input_dim(self) -> int:
+        return self._input_dim
+
+    def get_output_dim(self) -> int:
+        return self._input_dim
+
+    def forward(
+        self,
+        inputs: torch.Tensor,
+        mask: Optional[torch.BoolTensor] = None,
+    ) -> torch.Tensor:
+        return masked_pool(inputs, mask, self._method)
