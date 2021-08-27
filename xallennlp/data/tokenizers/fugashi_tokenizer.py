f4dad16575ea023153c502a1964df43aa89c5cd9
altescy <altescy@fastmail.com>
2021-08-27 22:45:53 +0900

    import fugashi inside of __init__

--- xallennlp/data/tokenizers/fugashi_tokenizer.py
+++ xallennlp/data/tokenizers/fugashi_tokenizer.py
@@ -2,7 +2,6 @@ from typing import List
 
 from allennlp.data.tokenizers.token_class import Token
 from allennlp.data.tokenizers.tokenizer import Tokenizer
-from fugashi.fugashi import Node, Tagger
 
 
 @Tokenizer.register("fugashi")
@@ -10,16 +9,18 @@ class FugashiTokenizer(Tokenizer):
     def __init__(
         self,
     ) -> None:
+        from fugashi.fugashi import Tagger
+
         super().__init__()
+
         self._tokenizer = Tagger()
 
     def tokenize(self, text: str) -> List[Token]:
-        return [self._convert_token(fugashi_token) for fugashi_token in self._tokenizer(text)]
-
-    def _convert_token(self, fugashi_token: Node) -> Token:
-        token = Token(
-            text=fugashi_token.surface,
-            lemma_=fugashi_token.feature.orthBase,
-            pos_=fugashi_token.feature.pos1,
-        )
-        return token
+        return [
+            Token(
+                text=fugashi_token.surface,
+                lemma_=fugashi_token.feature.orthBase,
+                pos_=fugashi_token.feature.pos1,
+            )
+            for fugashi_token in self._tokenizer(text)
+        ]
