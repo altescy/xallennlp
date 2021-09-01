04113d656f66eb27fa0ade608b886c656e11c2c2
altescy <altescy@fastmail.com>
2021-09-01 15:11:34 +0900

    add / fix is_bidirectional method

--- tests/modules/seq2seq_encoders/test_fnet_encoder.py
+++ tests/modules/seq2seq_encoders/test_fnet_encoder.py
@@ -4,6 +4,9 @@ from xallennlp.modules.seq2seq_encoders import FNetEncoder
 
 def test_fnet_encoder() -> None:
     inputs = torch.rand(8, 5, 16)
+
     encoder = FNetEncoder(16, 4, 32, positional_encoding="sinusoidal")
+    assert encoder.is_bidirectional()
+
     output = encoder(inputs)
     assert output.size() == (8, 5, 16)
