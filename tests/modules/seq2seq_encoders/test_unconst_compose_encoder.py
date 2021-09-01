d0d1017d44c48b9a96b6d9ba81ff640155460f53
altescy <altescy@fastmail.com>
2021-09-01 15:01:45 +0900

    add unconst compose encoder

---
+++ tests/modules/seq2seq_encoders/test_unconst_compose_encoder.py
@@ -0,0 +1,34 @@
+import torch
+from allennlp.modules.feedforward import FeedForward
+from allennlp.modules.seq2seq_encoders import FeedForwardEncoder, LstmSeq2SeqEncoder
+from allennlp.nn.activations import SwishActivation
+from xallennlp.modules.seq2seq_encoders import UnconstComposeEncoder
+
+
+def test_unconst_compose_encoder() -> None:
+    inputs = torch.rand(2, 3, 4)
+    encoder = UnconstComposeEncoder(
+        [
+            LstmSeq2SeqEncoder(
+                input_size=4,
+                hidden_size=3,
+                num_layers=1,
+                bidirectional=True,
+            ),
+            FeedForwardEncoder(
+                FeedForward(
+                    input_dim=4,
+                    hidden_dims=3,
+                    num_layers=2,
+                    activations=SwishActivation(),  # type: ignore
+                )
+            ),
+        ]
+    )
+
+    assert encoder.is_bidirectional()
+    assert encoder.get_input_dim() == 4
+    assert encoder.get_output_dim() == 9
+
+    output = encoder(inputs)
+    assert output.size() == (2, 3, 9)
