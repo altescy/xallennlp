7d699cf06f22d9839bd57811aff46550783a80fa
altescy <altescy@fastmail.com>
2022-01-17 18:07:32 +0900

    add concat seq2vec encoder

--- xallennlp/modules/seq2vec_encoders/__init__.py
+++ xallennlp/modules/seq2vec_encoders/__init__.py
@@ -1,2 +1,3 @@
 from xallennlp.modules.seq2vec_encoders.compose_seq2vec_encoder import ComposeSeq2VecEncoder  # noqa: F401
+from xallennlp.modules.seq2vec_encoders.concat_seq2vec_encoder import ConcatSeq2VecEncoder  # noqa: F401
 from xallennlp.modules.seq2vec_encoders.pooling_sec2vec_encoder import PoolingSeq2VecEncoder  # noqa: F401
