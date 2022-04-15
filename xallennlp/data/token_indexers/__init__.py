e156267751b29c75a0be51401b8d3010d80f8e1e
altescy <altescy@fastmail.com>
2022-04-15 15:06:54 +0900

    add fasttext subword indexer & embedder

--- xallennlp/data/token_indexers/__init__.py
+++ xallennlp/data/token_indexers/__init__.py
@@ -1,3 +1,4 @@
+from xallennlp.data.token_indexers.fasttext_subword_token_indexer import FastTextSubwordTokenIndexer  # noqa: F401
 from xallennlp.data.token_indexers.fasttext_token_indexer import FastTextTokenIndexer  # noqa: F401
 from xallennlp.data.token_indexers.minhash_token_indexer import MinHashTokenIndexer  # noqa: F401
 from xallennlp.data.token_indexers.pretrained_transformer_minhash_indexer import (  # noqa: F401
